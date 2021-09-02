858405f642e70b705185cf16101d5099fee28e6a
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2021-09-02 18:18:43 +0200

    Fix import path

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const plugin = require('tailwindcss/plugin')
-const generator = require('./generator')
+const generator = require('./src/generator')
 
 module.exports = plugin.withOptions(({ path = 'safelist.txt', patterns = [] }) => ({ theme }) =>
     fs.writeFileSync(path, generator(theme)(patterns).join('\n')))
